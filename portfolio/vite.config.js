6e6d6265a87f8d0d5b54c1a44e76c14e9fef2833
Thomas Alex <thomas321alex@gmail.com>
2025-08-23 14:47:21 +0100

    Added Netlify toml

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,8 +3,20 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: { open: true, port: 5173 }
+  base: '/',
+  server: { open: true, port: 5173 },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 })
 
 
